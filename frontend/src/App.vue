56b24518b076d3e90bb2b78839d57f3c0ac92d3a
Alexis Brones <alexis.brones@zenika.com>
2020-03-20 18:27:40 +0100

    Add home button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,22 @@
 <template>
-  <div id="app">
-    <!-- <div id="nav"> TODO: Icon for home button or navigation 
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <router-view />
-  </div>
+    <div id="app">
+        <router-link class="home-btn" to="/">
+            <h1>Remote Heath Check</h1>
+        </router-link>
+        <router-view />
+    </div>
 </template>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.home-btn {
+    text-decoration: none;
+    color: rgb(16, 124, 124);
 }
 </style>
